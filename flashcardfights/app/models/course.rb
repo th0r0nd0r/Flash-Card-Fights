3e716b434383906ce3a1079b18c8ad0630d386b1
Tyler Wood <the.tyler.wood@gmail.com>
2017-12-09 13:28:06 -0800

    course controller init

--- flashcardfights/app/models/course.rb
+++ flashcardfights/app/models/course.rb
@@ -10,4 +10,22 @@
 #
 
 class Course < ApplicationRecord
+  
+  def create
+    @course = Course.create(course_params)
+    if @course.save!
+      render "api/courses/show"
+    else
+      render @course.errors.full_messages, status: 422
+    end
+    
+  end
+  
+  private
+  
+  def course_params
+    params.require(:course).permit(:title, :educator_id)
+  end
+  
+  
 end

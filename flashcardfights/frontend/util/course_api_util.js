ef8624e74733fed5303cddb72e749771c209e90a
= <pjfritch1@gmail.com>
2017-12-09 15:57:50 -0800

    please work

---
+++ flashcardfights/frontend/util/course_api_util.js
@@ -0,0 +1,34 @@
+export const fetchCourses = data =>
+  $.ajax({
+    method: "GET",
+    url: "api/courses",
+    data,
+    err: () => console.log("Error fetching courses")
+  });
+
+export const fetchCourse = id =>
+  $.ajax({
+    method: "GET",
+    url: `api/courses/${id}`,
+    err: () => console.log("Error fetching course")
+  });
+
+export const createCourse = course =>
+  $.ajax({
+    method: "POST",
+    url: "api/courses",
+    data: course
+  });
+
+export const updateCourse = course =>
+  $.ajax({
+    method: "PATCH",
+    url: `api/courses/${course.course.id}`,
+    data: course
+  });
+
+export const deleteCourse = id =>
+  $.ajax({
+    method: "DELETE",
+    url: `api/courses/${id}`
+  });

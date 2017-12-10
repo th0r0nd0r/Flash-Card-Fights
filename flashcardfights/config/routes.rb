1f66d9ba599517d68c7fb87c34fa65cd68c544a4
Eric Tung <eric.p.tung@gmail.com>
2017-12-10 11:28:28 -0800

    Fix routes

--- flashcardfights/config/routes.rb
+++ flashcardfights/config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create, :update, :show]
     resource :session, only: [:new, :create, :destroy]
-    resources :questions, only: [:new, :create, :index, :show, :update, :destroy]
     resources :courses, only: [:new, :create, :index, :show, :update, :destroy]
     resources :quizzes, only: [:new, :create, :index, :show, :update, :destroy] do
       resources :quiz_attempts, only: [:create, :destroy, :show]
     end
-    resources :challenges, 
+    resources :challenges, only: [:create, :index, :show, :update, :destroy]
     resources :subjects, only: [:new, :create, :index, :show, :update, :destroy]
     resources :questions, only: [:new, :create, :index, :show, :update, :destroy] do
       resources :question_attempts, only: [:create, :destroy, :show]

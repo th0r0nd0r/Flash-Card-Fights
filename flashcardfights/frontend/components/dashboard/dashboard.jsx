e85affa33f0a7fbae02d34dbda07cf790ce775d9
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-12-10 10:42:13 -0800

    Finish filtering

--- flashcardfights/frontend/components/dashboard/dashboard.jsx
+++ flashcardfights/frontend/components/dashboard/dashboard.jsx
@@ -8,7 +8,9 @@ class Dashboard extends React.Component {
     super(props);
     this.subjectClick = this.subjectClick.bind(this);
     this.state = {
-      courses: []
+      allCourses: {},
+      courses: {},
+      filters: {}
     };
   }
 
@@ -18,29 +20,64 @@ class Dashboard extends React.Component {
   }
 
   componentWillReceiveProps(props) {
-    this.setState({ courses: props.courses });
+    this.setState({ allCourses: props.courses, courses: props.courses });
   }
 
   subjectClick(e) {
     e.preventDefault();
-    console.log(e.target.classList);
     if (e.target.classList.contains("tags-active")) {
       e.target.classList.remove("tags-active");
+      let newFilters = {};
+      let counter = 1;
+      Object.keys(this.state.filters).map((filter_indx) => {
+        if (this.state.filters[filter_indx] !== e.target.dataset.subject) {
+          newFilters[counter] = this.state.filters[filter_indx];
+          counter ++;
+        }
+      });
+      let filteredCourses = {};
+      let counter2 = 1;
+      Object.keys(this.state.allCourses).map((course_idx) => {
+        Object.values(this.state.allCourses[course_idx].subjects).map((x) => {
+          if (Object.values(newFilters).includes(x.title) && !Object.keys(filteredCourses).includes(course_idx)) {
+            filteredCourses[counter2] = this.state.allCourses[course_idx];
+            counter2++;
+          }
+        });
+      });
+      console.log(newFilters);
+      if (Object.keys(newFilters).length === 0) {
+        this.setState({ courses: this.state.allCourses });
+        this.setState({ filters: newFilters });
+      } else {
+        this.setState({filters: newFilters});
+        this.setState({ courses: filteredCourses });
+      }
     } else {
       e.target.classList.add("tags-active");
       let filteredCourses = {};
       const filter = e.target.dataset.subject;
+      const newFilters = {};
+      let counter2 = 1;
+      Object.keys(this.state.filters).map((filter_indx) => {
+        newFilters[counter2] = this.state.filters[filter_indx];
+        counter2++;
+      });
+      newFilters[counter2] = filter;
+      this.setState({ filters: newFilters });
       let counter = 1;
-      Object.keys(this.state.courses).map((course_idx) => {
-        Object.values(this.state.courses[course_idx].subjects).map((x) => {
-          if (x.title === filter) {
-            filteredCourses[counter] = this.state.courses[course_idx];
+      Object.keys(this.state.allCourses).map((course_idx) => {
+        Object.values(this.state.allCourses[course_idx].subjects).map((x) => {
+          if (Object.values(newFilters).includes(x.title) && !Object.keys(filteredCourses).includes(course_idx)) {
+            filteredCourses[counter] = this.state.allCourses[course_idx];
             counter ++;
           }
         });
       });
+      console.log("FILTERED COURSES", filteredCourses);
       this.setState({ courses: filteredCourses });
     }
+    console.log("state", this.state.filters);
   }
   
   render() {

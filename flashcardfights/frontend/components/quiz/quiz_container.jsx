0818e873afda91e4fd2e407da39d40a83a94e4fc
Eric Tung <eric.p.tung@gmail.com>
2017-12-10 12:15:58 -0800

    User index up and running, includes some fake testing route to be deleted later

--- flashcardfights/frontend/components/quiz/quiz_container.jsx
+++ flashcardfights/frontend/components/quiz/quiz_container.jsx
@@ -3,7 +3,7 @@ import Quiz from './quiz';
 import { getQuiz } from '../../actions/quiz_actions';
 import { createQuestionAttempt } from '../../actions/question_attempt_actions';
 
-console.log("get Quiz: ", getQuiz);
+// console.log("get Quiz: ", getQuiz);
 
 const mapStateToProps = (state, ownProps) => {
   console.log(state);
--- flashcardfights/frontend/reducers/entities_reducer.js
+++ flashcardfights/frontend/reducers/entities_reducer.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 import courses from "./courses_reducer";
 import quizAccoutrements from "./quizzes_reducer";
 import subjects from "./subject_reducer";
+import users from './users_reducer';
 // import challenge from "./challenge_reducer";
 
 export default combineReducers({
-  courses, quizAccoutrements, subjects,
+  courses, quizAccoutrements, subjects, users
   // challenge
 });

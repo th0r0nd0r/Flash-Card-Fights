af10c5481656af5d2c91ab8f7b0a3af6bfe91a25
= <pjfritch1@gmail.com>
2017-12-09 17:37:43 -0800

    i got it i got it

--- flashcardfights/db/seeds.rb
+++ flashcardfights/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demo_users = User.create([
+  {
+    username: 'Captain Kirk',
+    password: 'captainslog',
+    img_url: 'http://pixel.nymag.com/imgs/daily/vulture/2015/07/26/26-Kirk.w529.h529.jpg'
+  },
+  {
+    username: 'Jack Sparrow',
+    password: 'whynorum',
+    img_url: 'https://lumiere-a.akamaihd.net/v1/images/bluesteel_d0f846ee.jpeg?region=0%2C0%2C1580%2C880'
+  },
+  {
+    username: 'John Madden',
+    password: 'football',
+    img_url: 'https://cbssanfran.files.wordpress.com/2010/09/johnmadden01-250.jpg?w=318&h=318&crop=1'
+  },
+  {
+    username: 'Whoopee Goldberg',
+    password: 'crazydrinks',
+    img_url: 'http://www.treknews.net/wp-content/uploads/2016/08/whoopi-goldberg-guinan-star-trek-tng.jpg'
+  }
+])
+

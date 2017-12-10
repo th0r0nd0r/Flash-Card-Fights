b3707e2ac8742e4145dde4d9529f6821c819bbce
= <pjfritch1@gmail.com>
2017-12-10 13:29:24 -0800

    chchchchchanges

--- flashcardfights/frontend/actions/course_actions.js
+++ flashcardfights/frontend/actions/course_actions.js
@@ -1,7 +1,7 @@
 import * as APIUtil from "../util/course_api_util";
 
-export const RECEIVE_COURSES = "RECEIVE_courseS";
-export const RECEIVE_COURSE = "RECEIVE_course";
+export const RECEIVE_COURSES = "RECEIVE_COURSES";
+export const RECEIVE_COURSE = "RECEIVE_COURSE";
 export const RECEIVE_COURSE_ERRORS = "RECEIVE_COURSE_ERRORS";
 
 export const receiveCourses = courses => ({
